e7b432915c3488ab96bdf1dfdf3d5d2cac9f1647
Brand <macbookpro@brand-2.local>
2021-12-02 16:20:17 +0700

    Update layout

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -7,9 +7,6 @@ const App = () => {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/layouts/App.js</code> and save to reload.
-        </p>
       </header>
     </div>
   )
